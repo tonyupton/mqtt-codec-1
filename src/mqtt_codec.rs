f396b7f326a20d8e36eb0b105799ba566835b0f1
Tony <13627984+uptona@users.noreply.github.com>
2023-07-01 08:58:41 -0400

    modified:   src/mqtt_codec.rs

--- src/mqtt_codec.rs
+++ src/mqtt_codec.rs
@@ -547,11 +547,9 @@ impl<'a> MqttWriter<'a> {
         Ok(())
     }
 
-    pub fn write_binary_data(&mut self, data: &BinaryData) -> Result<(), ReasonCode> {
+    pub fn write_binary_data(&mut self, data: BinaryData) -> Result<(), ReasonCode> {
         self.write_two_byte_integer(data.len() as TwoByteInteger)?;
-        for byte in data {
-            self.write_byte(*byte)?;
-        }
+        data.iter().try_for_each(|byte| self.write_byte(*byte))?;
         Ok(())
     }
 }
@@ -706,54 +704,51 @@ impl ConnectPacket {
         }
     }
 
-    fn calculate_variable_header_length(&self) -> usize {
+    fn calculate_property_length(&self) -> usize {
         let mut length = 0;
-        let mut property_count = 0;
-        length += self.protocol_name.len() + 2; // protocol name length
-        length += 1; // protocol version
-        length += 1; // connect flags
-        length += 2; // keep alive
         if let Some(_) = self.session_expiry_interval {
-            property_count += 1;
-            length += 4;
+            length += 5;
         }
         if let Some(_) = self.receive_maximum {
-            property_count += 1;
-            length += 2;
+            length += 3;
         }
         if let Some(_) = self.maximum_packet_size {
-            property_count += 1;
-            length += 4;
+            length += 5;
         }
         if let Some(_) = self.topic_alias_maximum {
-            property_count += 1;
-            length += 2;
+            length += 3;
         }
         if let Some(_) = self.request_response_information {
-            property_count += 1;
-            length += 1;
+            length += 2;
         }
         if let Some(_) = self.request_problem_information {
-            property_count += 1;
-            length += 1;
+            length += 2;
         }
         if let Some(p) = &self.user_properties {
             for (key, value) in p {
-                property_count += 1;
                 length += key.len() + 2;
                 length += value.len() + 2;
             }
         }
         if let Some(a) = &self.authentication_method {
-            property_count += 1;
             length += a.len() + 2;
         }
         if let Some(d) = &self.authentication_data {
-            property_count += 1;
             length += d.len() + 2;
         }
-        length += sizeof_variable_byte_integer(property_count);
         length
+    }
+
+    fn calculate_variable_header_length(&self) -> (usize, usize) {
+        let mut length = 0;
+        length += self.protocol_name.len() + 2; // protocol name length
+        length += 1; // protocol version
+        length += 1; // connect flags
+        length += 2; // keep alive
+        let property_length = self.calculate_property_length();
+        length += sizeof_variable_byte_integer(property_length as u32);
+        length += property_length;
+        (length, property_length)
     }
 
     fn calculate_payload_length(&self) -> usize {
@@ -778,7 +773,7 @@ impl ConnectPacket {
     pub fn write_to_buffer(&self, buffer: &mut [Byte]) -> Result<usize, ReasonCode> {
         let mut writer = MqttWriter::new(buffer);
         writer.write_byte(0x10)?;
-        let variable_header_length = self.calculate_variable_header_length();
+        let (variable_header_length, property_length) = self.calculate_variable_header_length();
         let payload_length = self.calculate_payload_length();
         let remaining_length = variable_header_length + payload_length;
         writer.write_variable_byte_integer(remaining_length as VariableByteInteger)?;
